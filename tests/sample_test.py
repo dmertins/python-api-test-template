83ef1b8702d0eb03f4fbefd19a8940a9296d5153
Daniel Alves Mertins <dalvesmertins@gmail.com>
2023-06-27 21:05:50 -0300

    Add JSON schema validation test sample

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,2 +1,6 @@
+from validators.json_schema import json_matches_schema
+
+
 def test_something():
-    pass
+    assert json_matches_schema({"name": "Eggs", "price": 34.99}, "sample.json")
+    assert json_matches_schema({"name": "Eggs", "price": "Invalid"}, "sample.json")
